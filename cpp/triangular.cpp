d6485ec822a859288b174de7eeaa8118da654e51
Patrick Henehan <henehanpatrick0@gmail.com>
2023-05-07 18:34:08 -0400

    Solution to #12 -- highly divisible triangular num

---
+++ cpp/triangular.cpp
@@ -0,0 +1,41 @@
+# include <vector>
+# include <iostream>
+# include <ctime>
+
+using namespace std;
+
+int factor_pairs(long int N) {
+    long int i = 1;
+    long int j = N;
+    int f_cnt = 0;
+    while (i <= j) {
+        if (N % i == 0 && N % j == 0) {
+            f_cnt ++;
+        }
+        i++;
+        j = N / i;
+    };
+    return f_cnt;
+};
+ 
+int main() {
+    time_t  t1 = time(0);
+    int max_factors = 0;
+    long int X;
+    int factors;
+    for (long int i=1; i<1000000; i++) {
+        X = i*i + i;
+        factors = factor_pairs(X);
+        if (factors > max_factors) {
+            max_factors = factors;
+        };
+        if (max_factors >= 500) {
+            // cout << i << ' ' << X << ' ' << ' ' << X / 2 << '\n';
+            cout << "result: " << X / 2 << '\n';
+            time_t  t2 = time(0);
+            cout << "seconds elapsed: " << t2 - t1;
+            return 0;
+        };
+    };
+    return 0;
+};
